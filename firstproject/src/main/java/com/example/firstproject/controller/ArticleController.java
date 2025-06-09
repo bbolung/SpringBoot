580914c820907be46f6c242e8810933a51f6acee
bbolung <shoh2802@gmail.com>
2025-06-09 17:52:46 +0900

    *

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,14 +1,25 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @Slf4j
 public class ArticleController {
+
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -17,11 +28,51 @@ public class ArticleController {
     }
 
     @PostMapping("/articles/create")
-    public String createArticle(ArticleForm articleForm){
-        int age = 20;
+    public String createArticle(ArticleForm form){
         log.info("New article created");
-        log.info("articleForm: {}, age: {}", articleForm, age);
+        log.info("articleForm: {}", form);
 
-        return null;
+        // 1. DTO를 Entity로 변환
+        Article article = form.toEntity();
+
+        // 2. Repository 이용하여 Entity를 DB에 저장
+        Article saved = articleRepository.save(article);
+        log.info("New article: {}", saved);
+
+        return "redirect:/articles/" + saved.getId();
+    }
+    
+    //단일 데이터 조회
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable("id") Long id, Model model) {
+        log.info("Show article");
+
+        // 1. {id}값을 DB에서 꺼내오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);     //id값 조회(반환타입: Optional)
+        log.info("articleEntity: {}", articleEntity);
+
+        // 2. Entity -> DTO 변환
+        //필요하지만 우선 생략
+        
+        //3. view 전달
+        model.addAttribute("article", articleEntity);
+        
+        return "articles/show";     //화면 전달
+    }
+
+    //전체 데이터 조회
+    @GetMapping("/articles")
+    public String index(Model model) {
+
+        // 1. 모든 데이터 가져오기
+        List<Article> articleEntityList = articleRepository.findAll();
+
+        articleEntityList.forEach(list -> {log.info("list: {}", list);});
+
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+
+        // 3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
--- firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,5 +1,6 @@
 package com.example.firstproject.dto;
 
+import com.example.firstproject.entity.Article;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,4 +15,7 @@ public class ArticleForm {
     private String title;
     private String content;
 
+    public Article toEntity() {
+        return new Article(null, title, content);
+    }
 }
